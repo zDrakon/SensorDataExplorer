d62622e74b62e6cf34a587b639335a1dddc07a06
EvanYap <evanyap.14@gmail.com>
2016-11-09 07:05:23 -0800

    add new data files given, fix evan's

--- src/Tester.java
+++ src/Tester.java
@@ -1,15 +1,24 @@
 public class Tester {
 
-	public static double[][] sampleData;
-
-	public static String datafile = "data/pocketwalk_15steps_after_5_seconds.csv";
+	public static String[] datafiles = { "data/Alejandro/20StepsLeftHand.csv", "data/Alejandro/20StepsRightAnkle.csv",
+			"20StepsRightBackPocket.csv", "data/Evan/anklewalk_11steps_after5seconds_before5seconds_straightpath.csv",
+			"data/Evan/anklewalk_31steps_after5seconds_withturns.csv", "data/armwalk_40steps_after_5_seconds.csv",
+			"handwalk_17steps_after_5_seconds.csv", "data/Gina/back pocket 32.csv", "data/Gina/left hand 32.csv",
+			"right ankle 32.csv", "right arm 32.csv", "right thigh 32.csv" };
 
 	public static String videofile = "data/walkingSampleData.mp4";
 
 	public static void main(String[] args) {
+		doITAgain(19);
+	}
 
-		// Create data set
+	public static void doITAgain(int i) {
 
+	}
+
+	// Create data set
+	public static void doIt(String datafile, int n) {
+		double[][] sampleData;
 		CSVData dataset = new CSVData(datafile, 1);
 
 		sampleData = dataset.getData();
@@ -22,22 +31,15 @@ public class Tester {
 		double[] times = dataset.getColumn(0);
 
 		double[][] accelerationData = ArrayHelper.extractColumns(sampleData, new int[] { 1, 2, 3 });
-		double[] counts = StepCounter.countSteps(times, accelerationData, 2);
-
-		double[][] correspondStepsToTime = ArrayHelper.combineAsColumns(times, counts);
-
-		// DataExplorer.runDataExplorer(displayData, new String[] { "time",
-
-		// "mags" }, videofile);
-
-		// System.out.println(Arrays.toString(ArrayHelper.extractColumn(correspondStepsToTime,
-		// 0)) + "\n"
+		// for (int i = 0; i < 1000; i++) {
+		double[] counts = StepCounter.countSteps(times, accelerationData, n);
+		// if (StepCounter.numSteps(counts) == 11) {
+		// System.out.println(i);
 		//
-		// + Arrays.toString(ArrayHelper.extractColumn(correspondStepsToTime,
-		// 1)));
-		//
-		// System.out.println(StepCounter.numSteps(counts));
+		// }
+		// }
 
+		System.out.println(StepCounter.numSteps(counts));
 	}
 
 }
